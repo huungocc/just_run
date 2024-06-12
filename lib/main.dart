1973501c064f1a037618523a9d2f93189868d43b
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-12 11:23:49 +0700

    Colours & Fonts

--- lib/main.dart
+++ lib/main.dart
@@ -3,15 +3,37 @@ import 'package:flutter/material.dart';
 void main() => runApp(MaterialApp(
   home: Scaffold(
     appBar: AppBar(
-      title: Text('FIRST APP',),
+    title: Text(
+      'FIRST APP',
+      style: TextStyle(
+        fontFamily: 'Caveat Variable',
+        color: Colors.white,
+      ),
+    ),
       centerTitle: true,
+      backgroundColor: Colors.red[400],
     ),
     body: Center(
-      child: Text('hello, my name is Ngoc'),
+      child: Text(
+        'hello, my name is Ngoc',
+        style: TextStyle(
+          fontSize: 40,
+          fontWeight: FontWeight.bold,
+          letterSpacing: 2,
+          color: Colors.red[400],
+          fontFamily: 'Caveat Variable',
+        ),
+      ),
     ),
     floatingActionButton: FloatingActionButton(
-      child: Text('click'),
+      child: Text(
+        'click',
+        style: TextStyle(
+          color: Colors.white,
+      ),
+      ),
       onPressed: () {},
+      backgroundColor: Colors.red,
     ),
   ),
 ));
