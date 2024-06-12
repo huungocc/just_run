a737107311d24ff9fbbb734734be6ee0dfc23513
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-12 11:11:00 +0700

    Scaffold & AppBar Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,17 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Text('hello Ngoc!'),
+  home: Scaffold(
+    appBar: AppBar(
+      title: Text('FIRST APP',),
+      centerTitle: true,
+    ),
+    body: Center(
+      child: Text('hello, my name is Ngoc'),
+    ),
+    floatingActionButton: FloatingActionButton(
+      child: Text('click'),
+      onPressed: () {},
+    ),
+  ),
 ));
