300fa15c084841eceaf667700c0e1cdbdc8380ae
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-12 11:28:26 +0700

    Stateless Widgets & Hot Reload

--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,46 @@
 import 'package:flutter/material.dart';
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
-    appBar: AppBar(
-    title: Text(
-      'FIRST APP',
-      style: TextStyle(
-        fontFamily: 'Caveat Variable',
-        color: Colors.white,
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'FIRST APP',
+          style: TextStyle(
+            fontFamily: 'Caveat Variable',
+            color: Colors.white,
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: Colors.red[400],
       ),
-    ),
-      centerTitle: true,
-      backgroundColor: Colors.red[400],
-    ),
-    body: Center(
-      child: Text(
-        'hello, my name is Ngoc',
-        style: TextStyle(
-          fontSize: 40,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.red[400],
-          fontFamily: 'Caveat Variable',
+      body: Center(
+        child: Text(
+          'hello, my name is Ngoc',
+          style: TextStyle(
+            fontSize: 40,
+            fontWeight: FontWeight.bold,
+            letterSpacing: 2,
+            color: Colors.red[400],
+            fontFamily: 'Caveat Variable',
+          ),
         ),
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Text(
-        'click',
-        style: TextStyle(
-          color: Colors.white,
+      floatingActionButton: FloatingActionButton(
+        child: Text(
+          'click',
+          style: TextStyle(
+            color: Colors.white,
+          ),
+        ),
+        onPressed: () {},
+        backgroundColor: Colors.red,
       ),
-      ),
-      onPressed: () {},
-      backgroundColor: Colors.red,
-    ),
-  ),
-));
+    );
+  }
+}
