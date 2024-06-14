69b4bf5fe14b80de39c89742afdc2083bacf87f4
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-14 08:40:18 +0700

    part21

--- lib/main.dart
+++ lib/main.dart
@@ -1,47 +1,9 @@
 import 'package:flutter/material.dart';
-import 'quote.dart';
-import 'quotecard.dart';
+import 'package:ngocapp/pages/home.dart';
 
-void main() {
-  runApp(MaterialApp(
-    home: QuoteList(),
-  ));
-}
+void main() => runApp(MaterialApp(
+  home: Home(),
+));
 
-class QuoteList extends StatefulWidget {
-  @override
-  State<QuoteList> createState() => _QuoteListState();
-}
 
-class _QuoteListState extends State<QuoteList> {
-  List<Quote> quotes = [
-    Quote('Hello I am Ngoc', 'Tom'),
-    Quote('Welcome to my app', 'Tim'),
-    Quote('I live in Ha Noi, Viet Nam', 'Hela'),
-    Quote('Two things are infinite: the universe and human stupidity; and I am not sure about the universe', 'Albert Einstein')
-  ];
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.blueGrey,
-      appBar: AppBar(
-        title: Text(
-          'Awesome Quotes',
-          style: TextStyle(color: Colors.white),
-        ),
-        backgroundColor: Colors.blueGrey[800],
-      ),
-      body: Column(
-        children: quotes.map((quote) => QuoteCard(
-            quote: quote,
-            delete: () {
-              setState(() {
-                quotes.remove(quote);
-              });
-            }
-        )).toList(),
-      ),
-    );
-  }
-}
--- lib/quote.dart
+++
@@ -1,7 +0,0 @@
-class Quote {
-  String text;
-  String author;
-
-  //constructor
-  Quote(this.text, this.author);
-}
