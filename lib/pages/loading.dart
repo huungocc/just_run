172f62f8737aedf03af264f476e5b9761f35b318
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-17 10:18:57 +0700

    part32

--- lib/pages/loading.dart
+++ lib/pages/loading.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ngocapp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class Loading extends StatefulWidget {
   @override
@@ -28,9 +29,12 @@ class _LoadingState extends State<Loading> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: EdgeInsets.all(50),
-        child: Text('loading'),
+      backgroundColor: Colors.white,
+      body: Center(
+        child: SpinKitThreeBounce(
+          color: Colors.black,
+          size: 30,
+        )
       )
     );
   }
