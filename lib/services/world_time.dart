5a9c049e1bf8f5b24900e6dfe9d821a9c428a4ac
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-17 09:22:08 +0700

    part28

--- lib/services/world_time.dart
+++ lib/services/world_time.dart
@@ -2,20 +2,19 @@ import 'package:http/http.dart';
 import 'dart:convert';
 
 class WorldTime {
-  String location; // Tên địa điểm cho giao diện người dùng
+  String location; // Tên địa điểm
   String? time; // Thời gian tại địa điểm đó
   String flag; // URL đến biểu tượng cờ
-  String url; // Địa chỉ URL cho API endpoint
+  String url; // Địa chỉ cho endpoint
 
   WorldTime({required this.location, required this.flag, required this.url});
 
   Future<void> getTime() async {
-    try {
-      // Gửi yêu cầu đến API
+      // Gửi yêu cầu API
       Response response = await get(Uri.parse('http://worldtimeapi.org/api/timezone/$url'));
       Map data = jsonDecode(response.body);
 
-      // Lấy các thuộc tính từ dữ liệu
+      // Lấy dữ liệu
       String datetime = data['datetime'];
       String offset = data['utc_offset'].substring(1, 3);
 
@@ -23,11 +22,7 @@ class WorldTime {
       DateTime now = DateTime.parse(datetime);
       now = now.add(Duration(hours: int.parse(offset)));
 
-      // Gán giá trị cho thuộc tính time
+      // Gán giá trị cho time
       time = now.toString();
-    } catch (e) {
-      print('Lỗi bắt được: $e');
-      time = 'Không thể lấy dữ liệu thời gian';
-    }
   }
 }
