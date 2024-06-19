42ddef7053bee05089e23ef72d21744b12b8bfe8
Nguyen Huu Ngoc <ngcuchiha03@gmail.com>
2024-06-19 13:33:19 +0700

    pull to refresh & loading

--- lib/pages/choose_location.dart
+++ lib/pages/choose_location.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:ngocapp/services/world_time.dart';
+import 'package:flutter_spinkit/flutter_spinkit.dart';
 
 class ChooseLocation extends StatefulWidget {
   //khai bao bgColor
@@ -11,28 +12,40 @@ class ChooseLocation extends StatefulWidget {
 }
 
 class _ChooseLocationState extends State<ChooseLocation> {
-  List<WorldTime> locations = [];
+  static List<WorldTime> locations = [];
   List<WorldTime> filteredLocations = [];
+  bool isLoading = true;
 
   @override
   void initState() {
     super.initState();
-    // lay danh sach thanh pho
-    loadTimezones();
+    if (locations.isEmpty) {
+      loadTimezones();
+    } else {
+      setState(() {
+        filteredLocations = locations;
+        isLoading = false;
+      });
+    }
   }
 
   void loadTimezones() async {
+    setState(() {
+      isLoading = true;
+    });
+
     List<WorldTime> timezones = await WorldTime.getLocation();
     setState(() {
       locations = timezones;
       filteredLocations = timezones;
+      isLoading = false;
     });
   }
 
   void updateTime(index) async {
     WorldTime instance = filteredLocations[index];
     await instance.getTime();
-    Navigator.pop(context, {
+    Navigator.of(context).maybePop ({
       'location': instance.location,
       'time': instance.time,
       'isDayTime': instance.isDayTime
@@ -46,6 +59,10 @@ class _ChooseLocationState extends State<ChooseLocation> {
     setState(() {
       filteredLocations = filtered;
     });
+  }
+
+  Future <void> _refreshData() async {
+    loadTimezones();
   }
 
   @override
@@ -78,21 +95,31 @@ class _ChooseLocationState extends State<ChooseLocation> {
               ),
           ),
           Expanded(
-            child: ListView.builder(
-              itemCount: filteredLocations.length,
-              itemBuilder: (context, index){
-                return Padding(
-                  padding: const EdgeInsets.all(3.0),
-                  child: Card(
-                    child: ListTile(
-                      onTap: () {
-                        updateTime(index);
-                      },
-                      title: Text(filteredLocations[index].location, style: TextStyle(fontFamily: 'Anton')),
-                    ),
-                  ),
-                );
-              },
+            child: isLoading
+            ? Center(
+              child: SpinKitThreeBounce(
+                color: Colors.black,
+                size: 30,
+              )
+            )
+            : RefreshIndicator(
+              onRefresh: _refreshData,
+              child: ListView.builder(
+                  itemCount: filteredLocations.length,
+                  itemBuilder: (context, index){
+                    return Padding(
+                      padding: const EdgeInsets.all(3.0),
+                      child: Card(
+                        child: ListTile(
+                          onTap: () {
+                            updateTime(index);
+                          },
+                          title: Text(filteredLocations[index].location, style: TextStyle(fontFamily: 'Anton')),
+                        ),
+                      ),
+                    );
+                  },
+              ),
             ),
           ),
         ],
