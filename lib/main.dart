9d5bb14277060e4158efa0f9771b779555f5a51a
ngoc <ngcuchiha03@gmail.com>
2024-06-27 16:02:30 +0700

    login_logout

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,44 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
+import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+
 import 'package:just_run/pages/history.dart';
 import 'package:just_run/pages/login.dart';
 import 'package:just_run/pages/home.dart';
 import 'package:just_run/pages/result.dart';
 import 'package:just_run/pages/running.dart';
+import 'package:just_run/pages/loading.dart';
+import 'package:just_run/routes.dart';
 
-void main() => runApp(MaterialApp(
-    initialRoute: '/login',
-    routes: {
-      '/login': (context) => Login(),
-      '/home': (context) => Home(),
-      '/running': (context) => Running(),
-      '/history': (context) => History(),
-      '/result': (context) => Result(),
-    }
-));
+void main() async {
+      WidgetsFlutterBinding.ensureInitialized();
+      await Firebase.initializeApp();
+      runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+            return MaterialApp(
+                  initialRoute: Routes.loading,
+                  routes: {
+                        Routes.loading: (context) => Loading(),
+                        Routes.login: (context) => Login(),
+                        Routes.home: (context) => Home(),
+                        Routes.running: (context) => Running(),
+                        Routes.history: (context) => History(),
+                        Routes.result: (context) => Result(),
+                  },
+                  localizationsDelegates: [
+                        AppLocalizations.delegate,
+                        GlobalMaterialLocalizations.delegate,
+                        GlobalWidgetsLocalizations.delegate,
+                        GlobalCupertinoLocalizations.delegate,
+                  ],
+                  supportedLocales: [
+                        Locale('en'),
+                  ],
+            );
+      }
+}
---
+++ lib/routes.dart
@@ -0,0 +1,8 @@
+class Routes{
+  static const String loading = '/loading';
+  static const String login = '/login';
+  static const String home = '/home';
+  static const String running = '/running';
+  static const String result = '/result';
+  static const String history = '/history';
+}
